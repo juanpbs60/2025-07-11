2e2597e8bc388a5146e2edba23e0af916c5bfd2e
JPBS <juanpbs60@email.com>
2025-07-11 15:47:34 -0400

    primer actualizacion

--- 2-organizing-ideas(2).py
+++ 2-organizing-ideas(2).py
@@ -14,8 +14,6 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 
-##############################################################
-
 ########################  FILE DOWNLOADING  ########################
 
 DATASETS_DIR = 'datasets/' 
